ca27630cb10142186cbbeb3c2f5598f2f32c8eeb
aldominium <aldo2122@outlook.com>
2017-01-14 19:38:29 -0600

    Retrieves Hero List

--- app/src/main/java/com/aldominium/marvelheroes/MainActivity.java
+++ app/src/main/java/com/aldominium/marvelheroes/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aldominium.marvelheroes;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +21,12 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String HERO_LIST_FRAGMENT = "hero_list_fragment";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int SUCCESS_CODE = 200;
+
     private FrameLayout frameLayout;
+    private ArrayList<SuperHero> superHeros;
 
     public static final int AVENGERS_COMIC_ID = 354;
 
@@ -28,13 +35,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        frameLayout = (FrameLayout) findViewById(R.id.placeholder);
+
 
         Call<Basic<Data<ArrayList<SuperHero>>>> superHeroesCall = MarvelService.getMarvelApi().getHeroes(AVENGERS_COMIC_ID);
 
         superHeroesCall.enqueue(new Callback<Basic<Data<ArrayList<SuperHero>>>>() {
             @Override
             public void onResponse(Call<Basic<Data<ArrayList<SuperHero>>>> call, Response<Basic<Data<ArrayList<SuperHero>>>> response) {
-                Toast.makeText(MainActivity.this, "Hero Name: " + response.body().getData().getResults().get(0).getName(), Toast.LENGTH_SHORT).show();
+
+                if (response.code() == SUCCESS_CODE){
+
+                    superHeros = response.body().getData().getResults();
+
+                    Toast.makeText(MainActivity.this, "Hero Name: " + superHeros.get(0).getName(), Toast.LENGTH_SHORT).show();
+
+
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    HeroListFragment heroListFragment = new HeroListFragment();
+                    fragmentTransaction.add(R.id.placeholder,heroListFragment, HERO_LIST_FRAGMENT);
+                    fragmentTransaction.commit();
+
+
+                }else {
+
+                    Log.d(TAG, "Error en la respuesta");
+
+
+                }
+
+
+
+
 
             }
 
@@ -46,7 +79,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        frameLayout = (FrameLayout) findViewById(R.id.placeholder);
+
+
 
 
 
