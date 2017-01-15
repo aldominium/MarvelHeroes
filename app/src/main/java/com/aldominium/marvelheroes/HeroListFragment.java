353fab31ee34aa5412c6a3b2a386a0e8175989ef
aldominium <aldo2122@outlook.com>
2017-01-14 21:10:58 -0600

    Adds RecyclerView to the Fragment

--- app/src/main/java/com/aldominium/marvelheroes/HeroListFragment.java
+++ app/src/main/java/com/aldominium/marvelheroes/HeroListFragment.java
@@ -3,6 +3,7 @@ package com.aldominium.marvelheroes;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,8 @@ import java.util.ArrayList;
 
 
 public class HeroListFragment extends Fragment {
+
+    private static final String TAG = HeroListFragment.class.getSimpleName();
 
     ArrayList<SuperHero> superHeroes;
 
@@ -28,10 +31,15 @@ public class HeroListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         Bundle bundle = getArguments();
+
+
         superHeroes = bundle.getParcelableArrayList(MainActivity.HERO_LIST);
 
-        Toast.makeText(getContext(), "El primer superheroe es: " + superHeroes.get(0).getName(), Toast.LENGTH_SHORT).show();
+        if (superHeroes == null)
+            Log.d(TAG, "No hay superheroes en el bundle");
+
 
     }
 
